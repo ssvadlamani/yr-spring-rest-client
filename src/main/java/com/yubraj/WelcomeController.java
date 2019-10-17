0369c0a086751eec15720b8c93cbd808a40aed91
siva <siva.8563@gmail.com>
2019-10-17 23:46:44 +0530

    first commit

---
+++ src/main/java/com/yubraj/WelcomeController.java
@@ -0,0 +1,30 @@
+package com.yubraj;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		String s = restTemplate.getForObject("http://localhost:8080/employees/",String.class);
+		System.out.println("s======================>  "+s);
+		model.put("response", s);
+		return "welcome";
+	}
+
+}
